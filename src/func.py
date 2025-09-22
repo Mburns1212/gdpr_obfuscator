5ffc79afbfacb1750ac617469f74b9cce522cbf1
Mburns1212 <matthew.burns388@gmail.com>
2025-09-22 16:22:59 +0100

    add obfuscator function

---
+++ src/func.py
@@ -0,0 +1,17 @@
+import pandas
+from boto3 import client
+
+def obfuscator(event):
+    file = event['file_to_obfuscate']
+    piis = event['pii_fields']
+    s3_client = client('s3')
+    bucket_name = file.split('/')[2]
+    key = '/'.join(file.split('/')[3:])
+    csv_df = pandas.read_csv(s3_client.get_object(Bucket=bucket_name, Key=key)['Body'])
+    for column in csv_df:
+        if column in piis:
+            csv_df[column] = '***'
+    byte_csv = csv_df.to_csv(index=False).encode('utf-8')
+    return byte_csv
+
+
