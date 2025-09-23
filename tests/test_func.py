a0e7107ee761a4412a7d870d2a3257dc429a99a2
Mburns1212 <matthew.burns388@gmail.com>
2025-09-23 14:59:39 +0100

    create mock aws client and bucket, add tests

---
+++ tests/test_func.py
@@ -0,0 +1,49 @@
+import pytest
+from src.func import obfuscator
+from moto import mock_aws
+import os
+import boto3
+from unittest.mock import patch
+
+@pytest.fixture(autouse=True)
+def aws_creds():
+    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+    os.environ["AWS_SECURITY_TOKEN"] = "testing"
+    os.environ["AWS_SESSION_TOKEN"] = "testing"
+    os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
+
+@pytest.fixture(scope="function")
+def s3_client(aws_creds):
+    with mock_aws():
+        yield boto3.client("s3")
+
+@pytest.fixture(scope="function")
+def s3_client_with_bucket(s3_client):
+    s3_client.create_bucket(Bucket="test-bucket")
+    yield s3_client
+
+@pytest.fixture(scope="function")
+def s3_client_with_bucket_with_objects(s3_client_with_bucket):
+    with open('tests/test_files/test.csv','rb') as file:
+       s3_client_with_bucket.put_object(
+           Bucket="test-bucket",
+           Key="test.csv",
+           Body=file.read()
+       )
+    yield s3_client_with_bucket
+
+
+@patch('src.func.client')
+def test_returns_bytestream(mock_client, s3_client_with_bucket_with_objects):
+    mock_client.return_value = s3_client_with_bucket_with_objects
+    assert type(obfuscator({'file_to_obfuscate': 's3://test-bucket/test.csv', 'pii_fields': ['student_id']})) == bytes
+
+@patch('src.func.client')
+def test_obfuscates_piis(mock_client, s3_client_with_bucket_with_objects):
+    mock_client.return_value = s3_client_with_bucket_with_objects
+    result = obfuscator({'file_to_obfuscate': 's3://test-bucket/test.csv', 'pii_fields': ['student_id','course']}).decode('utf-8')
+    with open('tests/test_files/obfuscated.csv','rb') as file:
+        expected = file.read().decode('utf-8')
+    assert result == expected
+
