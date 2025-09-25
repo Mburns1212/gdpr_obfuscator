bce7cda2d676cb9bd5d39fd90e258c4c775ca477
Mburns1212 <matthew.burns388@gmail.com>
2025-09-25 13:40:41 +0100

    add tests for json files

--- tests/test_func.py
+++ tests/test_func.py
@@ -30,20 +30,39 @@ def s3_client_with_bucket_with_objects(s3_client_with_bucket):
            Bucket="test-bucket",
            Key="test.csv",
            Body=file.read()
-       )
+        )
+    with open('tests/test_files/test.json','rb') as file:
+        s3_client_with_bucket.put_object(
+            Bucket="test-bucket",
+            Key="test.json",
+            Body=file.read()
+        )
     yield s3_client_with_bucket
 
 
 @patch('src.func.client')
-def test_returns_bytestream(mock_client, s3_client_with_bucket_with_objects):
+def test_returns_bytestream_csv(mock_client, s3_client_with_bucket_with_objects):
     mock_client.return_value = s3_client_with_bucket_with_objects
     assert type(obfuscator({'file_to_obfuscate': 's3://test-bucket/test.csv', 'pii_fields': ['student_id']})) == bytes
 
 @patch('src.func.client')
-def test_obfuscates_piis(mock_client, s3_client_with_bucket_with_objects):
+def test_returns_bytestream_json(mock_client, s3_client_with_bucket_with_objects):
+    mock_client.return_value = s3_client_with_bucket_with_objects
+    assert type(obfuscator({'file_to_obfuscate': 's3://test-bucket/test.json', 'pii_fields': ['student_id']})) == bytes
+
+@patch('src.func.client')
+def test_obfuscates_piis_csv(mock_client, s3_client_with_bucket_with_objects):
     mock_client.return_value = s3_client_with_bucket_with_objects
     result = obfuscator({'file_to_obfuscate': 's3://test-bucket/test.csv', 'pii_fields': ['student_id','course']}).decode('utf-8')
     with open('tests/test_files/obfuscated.csv','rb') as file:
+        expected = file.read().decode('utf-8')
+    assert result == expected
+
+@patch('src.func.client')
+def test_obfuscates_piis_json(mock_client, s3_client_with_bucket_with_objects):
+    mock_client.return_value = s3_client_with_bucket_with_objects
+    result = obfuscator({'file_to_obfuscate': 's3://test-bucket/test.json', 'pii_fields': ['student_id']}).decode('utf-8')
+    with open('tests/test_files/obfuscated.json','rb') as file:
         expected = file.read().decode('utf-8')
     assert result == expected
 
