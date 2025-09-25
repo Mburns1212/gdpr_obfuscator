28dbad953fe54f8d0c86a5057bf53c1d745e0d95
Mburns1212 <matthew.burns388@gmail.com>
2025-09-25 13:40:25 +0100

    add functionality for json files

--- src/func.py
+++ src/func.py
@@ -1,5 +1,6 @@
 import pandas
 from boto3 import client
+import json
 
 def obfuscator(event):
     file = event['file_to_obfuscate']
@@ -8,11 +9,20 @@ def obfuscator(event):
     bucket_name = file.split('/')[2]
     key = '/'.join(file.split('/')[3:])
     file_type = key.split('.')[-1]
-    csv_df = pandas.read_csv(s3_client.get_object(Bucket=bucket_name, Key=key)['Body'])
-    for column in csv_df:
-        if column in piis:
-            csv_df[column] = '***'
-    byte_csv = csv_df.to_csv(index=False).encode('utf-8')
-    return byte_csv
+    if file_type == 'csv':
+        csv_df = pandas.read_csv(s3_client.get_object(Bucket=bucket_name, Key=key)['Body'])
+        for column in csv_df:
+            if column in piis:
+                csv_df[column] = '***'
+        byte_csv = csv_df.to_csv(index=False).encode('utf-8')
+        return byte_csv
+    if file_type == 'json':
+        json_file = json.loads(s3_client.get_object(Bucket=bucket_name, Key=key)['Body'].read())
+        for item in json_file:
+            for field in item:
+                if field in piis:
+                    item[field] = '***'
+        byte_json = json.dumps(json_file,indent=2).encode('utf-8')
+        return byte_json
 
 
