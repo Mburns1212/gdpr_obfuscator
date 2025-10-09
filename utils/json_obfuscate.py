7a23f3479a3db5d1f594e90727934c23d7acae5a
Mburns1212 <matthew.burns388@gmail.com>
2025-10-09 15:54:53 +0100

    create recursive function to obfuscate json

---
+++ utils/json_obfuscate.py
@@ -0,0 +1,11 @@
+def json_obfuscate(json, piis):
+    if isinstance(json,dict):
+        for k, v in json.items():
+            if k in piis:
+                json[k] = '***'
+            else:
+                json_obfuscate(v, piis)
+            
+    if isinstance(json,list):
+        for item in json:
+            json_obfuscate(item, piis)
