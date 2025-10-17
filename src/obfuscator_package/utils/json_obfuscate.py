e19d9a67e783ec001575c4de38a5b550bb6e7e39
Mburns1212 <matthew.burns388@gmail.com>
2025-10-17 19:56:22 +0100

    reformat

--- src/obfuscator_package/utils/json_obfuscate.py
+++ src/obfuscator_package/utils/json_obfuscate.py
@@ -1,11 +1,11 @@
 def json_obfuscate(json, piis):
-    if isinstance(json,dict):
+    if isinstance(json, dict):
         for k, v in json.items():
             if k in piis:
                 json[k] = '***'
             else:
                 json_obfuscate(v, piis)
-            
-    if isinstance(json,list):
+
+    if isinstance(json, list):
         for item in json:
             json_obfuscate(item, piis)
