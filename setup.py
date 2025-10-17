1b5ccae46eee51e39f2620c129638d2fe74b5865
Mburns1212 <matthew.burns388@gmail.com>
2025-10-17 19:29:45 +0100

    add setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="gdpr_obfuscator",
     version="0.1.0",
-    author="Your Name",
+    author="Matthew Burns",
     description="A tool for obfuscating sensitive data in files stored on S3.",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
